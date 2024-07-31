67cadf234102cebec86043a4cc8f6e15678194ad
akihiro-kondo <2107sum@gmail.com>
2024-08-01 02:56:14 +0900

    feat: ログイン画面と登録画面の作成

--- myapp/app/routes.py
+++ myapp/app/routes.py
@@ -5,3 +5,11 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
