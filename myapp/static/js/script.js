0c2b88b9b35c71c0fb2c20017f2df060a5bca01e
akihiro-kondo <2107sum@gmail.com>
2024-08-06 22:16:12 +0900

    update: input.htmlのフロント改善

--- myapp/static/js/script.js
+++ myapp/static/js/script.js
@@ -10,14 +10,14 @@ function addField(field) {
     var newFieldCount = fieldsContainer.children.length + 1; // Number to ensure unique ID
 
     var newFieldHtml = `
-        <div>
-            <input type="text" name="${field}_${newFieldCount - 1}">
-            <input type="number" name="${field}_${newFieldCount - 1}_num">
+        <div class="input-group">
+            <input class="input input-name" type="text" name="${field}_${newFieldCount - 1}" placeholder="技術名">
+            <input class="input input-duration" type="number" name="${field}_${newFieldCount - 1}_num" placeholder="期間">
             <span>ヶ月</span>
         </div>
     `;
-    fieldsContainer.innerHTML += newFieldHtml;
-};
+    fieldsContainer.insertAdjacentHTML('beforeend', newFieldHtml);
+}
 
 
 
