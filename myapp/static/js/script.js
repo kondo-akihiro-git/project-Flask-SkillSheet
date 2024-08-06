1475652a33b9c827da5fd751e3a693db9adea85a
akihiro-kondo <2107sum@gmail.com>
2024-08-06 17:07:46 +0900

    update: フロント部分の画面デザインを修正

--- myapp/static/js/script.js
+++ myapp/static/js/script.js
@@ -19,65 +19,6 @@ function addField(field) {
     fieldsContainer.innerHTML += newFieldHtml;
 };
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// 
-// イベント：addField関数への仲介
-// 詳細：input画面やedit_project画面において、どの＋ボタンがクリックされているのか確認する
-// 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-document.getElementById('add-os').addEventListener('click', function(e) {
-    e.preventDefault();
-    addField('os');
-});
-
-document.getElementById('add-language').addEventListener('click', function(e) {
-    e.preventDefault();
-    addField('language');
-});
-
-document.getElementById('add-framework').addEventListener('click', function(e) {
-    e.preventDefault();
-    addField('framework');
-});
-
-document.getElementById('add-database').addEventListener('click', function(e) {
-    e.preventDefault();
-    addField('database');
-});
-
-document.getElementById('add-containertech').addEventListener('click', function(e) {
-    e.preventDefault();
-    addField('containertech');
-});
-
-document.getElementById('add-cicd').addEventListener('click', function(e) {
-    e.preventDefault();
-    addField('cicd');
-});
-
-document.getElementById('add-logging').addEventListener('click', function(e) {
-    e.preventDefault();
-    addField('logging');
-});
-
-document.getElementById('add-tools').addEventListener('click', function(e) {
-    e.preventDefault();
-    addField('tools');
-});
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// 
-// イベント：リンク無効化メッセージ
-// 詳細：リンク無効化メッセージを表示し、フォームの送信も防ぐ
-// 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// リンク無効化時の処理
-document.getElementById('invalidate-link-form').addEventListener('submit', function(event) {
-    if (!confirm('本当にリンクを無効にしますか？')) {
-        event.preventDefault();  // ユーザーがキャンセルした場合、フォームの送信を防ぐ
-    }
-});
 
 
 
